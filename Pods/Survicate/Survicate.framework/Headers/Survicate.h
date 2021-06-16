fc0120ac70f85e5debec581c0f674d6cee8d9fbf
Harry Li <zhgchgli@gmail.com>
2021-06-16 11:11:31 +0800

    update version to 1.5.0

--- Pods/Survicate/Survicate.framework/Headers/Survicate.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-//! Project version number for Survicate.
-FOUNDATION_EXPORT double SurvicateVersionNumber;
-
-//! Project version string for Survicate.
-FOUNDATION_EXPORT const unsigned char SurvicateVersionString[];
